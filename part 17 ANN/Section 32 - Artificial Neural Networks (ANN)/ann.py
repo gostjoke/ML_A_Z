df51ce172413212e3742b3095392fcc225a9fc88
Will <66586152+gostjoke@users.noreply.github.com>
2023-09-08 00:51:41 -0700

    Add files via upload

--- part 17 ANN/Section 32 - Artificial Neural Networks (ANN)/ann.py
+++ part 17 ANN/Section 32 - Artificial Neural Networks (ANN)/ann.py
@@ -21,17 +21,22 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
+
+# predict 
 y = dataset.iloc[:, 13].values
 
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X_1 = LabelEncoder()
-X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-labelencoder_X_2 = LabelEncoder()
-X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features = [1])
-X = onehotencoder.fit_transform(X).toarray()
-X = X[:, 1:]
+from sklearn.compose import ColumnTransformer
+
+le = LabelEncoder()
+# gender transform
+X[:, 2] = le.fit_transform(X[:, 2])
+
+# country transform
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -54,7 +59,7 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
-classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))#, input_dim = 11))
 
 # Adding the second hidden layer
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
@@ -75,5 +80,18 @@ y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
+
+# Extract the values from the confusion matrix
+tn, fp, fn, tp = cm.ravel()
+
+# Calculate accuracy
+accuracy = (tp + tn) / (tp + tn + fp + fn)
+
+print(f"Accuracy: {accuracy}")
+
+# or 
+print(accuracy_score(y_test, y_pred))
+
+classifier.save('Churn_Modelling.h5')
